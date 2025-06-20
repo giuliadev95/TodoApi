691da2add568f200976ecaf0cd6cde795543baa0
giulia_moukouyou <giuliamoukouyou@gmail.com>
2025-06-20 17:57:28 +0200

    Edit Program.cs writing logic for API calls - not completed.

--- Program.cs
+++ Program.cs
@@ -15,7 +15,25 @@ builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList")
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
+// Get all the tasks
 app.MapGet("/todoitems", async (TodoDb db) =>
-await db.Todos.TodoListAsync());
+await db.Todos.ToListAsync());
+
+// Get completed tasks, with a flag 'iscomplete'
+app.MapGet("todoitems/complete", async (TodoDb db) =>
+await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+// Get a task by ID
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+    is Todo todo
+    ? Results.Ok(todo)
+    : Results.NotFound());
+
+app.MapPost("/todoitems", async (Todo toto, TodoDb db) =>
+{
+
+}
+)
 
 
